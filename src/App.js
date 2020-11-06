ddcd14961ed7f9f30c2f07ca454910dd46742c13
Aaron Mathews <aamathews23@gmail.com>
2020-11-05 16:54:32 -0800

    Added documentation and a new unit test to the App.test.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+/**
+ * The App component is home to the other components PostList and Search. It will also handle all of the fetching from
+ * the Reddit JSON API.
+ * 
+ * Author: Aaron Mathews
+ * Date: 11/5/2020
+ */
 export const App = () => {
     return (
         <h1>Reddit Client</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { App } from "./App";
+import { PostList } from "./features/post-list/PostList";
 
+/**
+ * Tests the behavior of the App component.
+ * 
+ * Author: Aaron Mathews
+ * Date: 11/5/2020
+ */
 describe("App component", () => {
     it("Contains Reddit Client header", () => {
         const wrapper = shallow(<App />);
         const text = wrapper.find("h1").text();
 
         expect(text).toEqual("Reddit Client");
+    });
+
+    it("renders a PostList component", () => {
+        // Setup
+        const outcome = true;
+        const element = <PostList />
+
+        // Exercise
+        const wrapper = shallow(<App />);
+
+        // Verify
+        expect(wrapper.containsMatchingElement(element)).toEqual(outcome);
+
     });
 });
