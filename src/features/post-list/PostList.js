78a3849b553897bb93c0bb08204fcbca9bd007f9
Aaron Mathews <aamathews23@gmail.com>
2020-11-05 16:33:07 -0800

    Created a basic PostList component that renders a list of PostItems

--- src/features/post-list/PostList.js
+++ src/features/post-list/PostList.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./PostList.css";
+import { PostItem } from "../post-item/PostItem";
 
-export const PostList = () => {
+/**
+ * The PostList component will handle rendering the list of Reddit posts. It will also call the Reddit JSON API to retrieve the data.
+ * 
+ * Author: Aaron Mathews
+ * Date: 11/5/2020
+ */
+export const PostList = (props) => {
+    let posts = [];
+    
+    if (props.posts) {
+        posts = props.posts.map((post, index) => {
+            return <PostItem title={post.title} src={post.src} alt={post.alt} key={index} />
+        });
+    }
+
+
     return (
-        <h1>PostList</h1>
+        <div>
+            {posts}
+        </div>
     );
 };
--- src/features/post-list/PostList.test.js
+++ src/features/post-list/PostList.test.js
@@ -2,11 +2,59 @@ import React from "react";
 import { shallow } from "enzyme";
 import { PostList } from "./PostList";
 
+/**
+ * Tests to make sure the PostList component behaves correctly.
+ * 
+ * Author: Aaron Mathews
+ * Date: 11/5/2020
+ */
 describe("PostList component", () => {
-    it("renders a PostList h1", () => {
-        const wrapper = shallow(<PostList />);
-        const text = wrapper.find("h1").text();
+    it("renders a list of Reddit Posts", () => {
+        // Setup
+        const mockPosts = [
+            {
+                "title": "Post 1",
+                "src": "Src1.jpg",
+                "alt": "Alt 1"
+            },
+            {
+                "title": "Post 2",
+                "src": "Src2.jpg",
+                "alt": "Alt 2"
+            },
+            {
+                "title": "Post 2",
+                "src": "Src2.jpg",
+                "alt": "Alt 2"
+            },
+        ];
+        
+        // Exercise
+        const wrapper = shallow(<PostList posts={mockPosts} />);
 
-        expect(text).toEqual("PostList");
+        // Verify
+        expect(wrapper).toMatchSnapshot();
     }); 
+
+    it("doesn't render a list when not given one", () => {
+        // Exercise
+        const wrapper = shallow(<PostList />);
+
+        // Verify
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("doesn't break when given an empty list of posts", () => {
+        // Setup
+        const posts = [];
+        const element = "li";
+        const count = 0;        
+
+        // Exercise
+        const wrapper = shallow(<PostList posts={posts} />);
+        const li = wrapper.find(element);
+
+        // Verify
+        expect(li).toHaveLength(count);
+    });
 });
